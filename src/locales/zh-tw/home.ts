41ad001856812f6a3f36912461d68b93b49f08a1
Hall <oncemeet0857@gmail.com>
2025-10-15 09:24:17 +0800

    update

--- src/locales/zh-tw/home.ts
+++ src/locales/zh-tw/home.ts
@@ -4,10 +4,10 @@ export default {
   helloTitle: "我是 Hall",
   im: "我是",
   jobTitle: "全端工程師",
-  openingText: `我是一名有5~6年前端實戰開發經驗的前端工程師，也對後端有常用的運用及深刻理解，對網頁設計和開發有著強烈的熱情與技術支撐外，與程式語言相關方面都有極大的興趣與熱情。
-我精通 HTML、CSS、JavaScript、TypeScript、Vue、Vuex、Nuxt.js、Node.js、express、React。
-除了必備的git版本控制外，我也理解並且會運用PHP 、MYSQL等技術。
-甚至有使用Web Serial串接如入鈔機等的硬體機器的實際開發經驗。`,
+  openingText: `我是一位擁有 5 至 6 年前端開發經驗的工程師，對網頁設計與程式開發充滿熱情與實戰經驗。
+精通 HTML、CSS、JavaScript、TypeScript、Vue（含 Vuex、Nuxt.js），並熟悉 React、Node.js 等。
+除前端領域外，也具備 PHP 與 MySQL 的實作經驗，能獨立完成全端開發任務。
+熟悉 Git 版本控制，並曾實際開發 透過 Web Serial 串接硬體設備（如入鈔機） 的專案，具備跨領域整合能力與問題解決思維。`,
   button: "聯絡我",
   jobTitleAt: "前端工程師、後端工程師",
   company: "",
